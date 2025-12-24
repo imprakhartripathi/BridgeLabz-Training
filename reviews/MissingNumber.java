95a95f8dffdecdc46cdb2285a0044a0ddcba4d57
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-12-24 10:17:50 +0530

    [Prakhar Tripathi] Refactored - MissingNumber.java for cleanup

--- reviews/MissingNumber.java
+++ reviews/MissingNumber.java
@@ -1,39 +1,37 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class MissingNumber {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+
             int size = sc.nextInt();
             int[] arr = new int[size];
+
             System.out.println("Please Enter the array below in a sequence with one missing in between");
+
             for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            Arrays.sort(arr); // just sorting it because sometimes people make mistakes
+            Arrays.sort(arr); // sorting the array, just in case user enters unsorted array
 
             int start = arr[0];
-            int end = arr[size - 1];
-            int x = start;
 
-            int[] newarr = new int[size + 1];
-            while (x != end) {
-                for (int i = 0; i < size; i++) {
-                    newarr[i] = x;
-                    x++;
-                }
+            int[] newarr = new int[size];
+
+            // actual array creation
+            for (int i = 0; i < size; i++) {
+                newarr[i] = start + i;
             }
 
-            for(int i=0; i<size; i++){
-                if(arr[i] != newarr[i]){
+            // comparing the res to actully find the missing number
+            for (int i = 0; i < size; i++) {
+                if (arr[i] != newarr[i]) {
                     System.out.println("Missing no is " + newarr[i]);
                     break;
                 }
-                
             }
         }
-
     }
 }
