d88eee72ef37471c7f87bd29b1dc82ff84e7267b
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2026-01-27 11:41:23 +0530

    [Prakhar Tripathi] Added - Submission of Exceptions in GCR

---
+++ gcr-codebase/exceptions/MultipleCatchDemo.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class MultipleCatchDemo {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        int index = 5;
+
+        try {
+            System.out.println("Value: " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
